a9462ade2459b5897846393780987d5667b07798
Lucas Garron <code@garron.net>
2026-04-16 20:43:00 -0700

    Deduplicate the source root definition in scripts.

--- script/dev.ts
+++ script/dev.ts
@@ -1,7 +1,8 @@
 import { Builder } from "linkrel-ssg";
+import { SRC_ROOT } from "./build";
 
 const builder = new Builder({
-  srcRoot: "./src/garron.net/",
+  srcRoot: SRC_ROOT,
   outputDir: "./.cache/dev/web/garron.net/",
 });
 
